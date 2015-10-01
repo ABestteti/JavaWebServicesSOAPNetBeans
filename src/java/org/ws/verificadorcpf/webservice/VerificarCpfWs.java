45146e03d513e907c4375571d39016b92f9f6808
anderson.bestteti@gmail.com <anderson.bestteti@gmail.com>
2015-10-01 16:02:03 -0300

    Created the CPF validation class.

--- src/java/org/ws/verificadorcpf/webservice/VerificarCpfWs.java
+++ src/java/org/ws/verificadorcpf/webservice/VerificarCpfWs.java
@@ -22,7 +22,46 @@ public class VerificarCpfWs {
      */
     @WebMethod(operationName = "verificarCpf")
     public Boolean verificarCpf(@WebParam(name = "cpf") String cpf) {
+        int digitosCpf[] = new int[11];
+        int i, j, totalSoma, resultado, verificador1, verificador2;
         
+        // Efetua a conversao do CPF, de string para digitos numericos
+        for (i = 0; i < 11; i++) {
+            digitosCpf[i] = cpf.charAt(i) - 48;
+        }
+        
+        // Calcula o primeiro digito verificador
+        totalSoma = 0;
+        for (i =0, j = 10; i < 9; i++, j--) {
+            totalSoma += (digitosCpf[i] * j);
+        }
+        resultado = totalSoma % 11;
+        if ((resultado == 0) || (resultado == 1)) {
+            verificador1 = 0;            
+        }
+        else {
+            verificador1 = 11 - resultado;
+        }
+        
+        // Calcula o segundo digito verificador
+        totalSoma = 0;
+        for (i =0, j = 11; i < 9; i++, j--) {
+            totalSoma += (digitosCpf[i] * j);
+        }
+        totalSoma += (verificador1 * 2);
+        resultado = totalSoma % 11;
+        if ((resultado == 0) || (resultado == 1)) {
+            verificador2 = 0;            
+        }
+        else {
+            verificador2 = 11 - resultado;
+        }
+        
+        // Verifica se o CPF e valido
+        if ((verificador1 == digitosCpf[9]) &&
+            (verificador2 == digitosCpf[10])) {
+            return true;
+        }
         return false;
     }
 }
